b2b207c3dff8bbc9374605ad1ac41890d4d107ab
skeleton625 <chaos0824@naver.com>
2018-08-14 23:56:21 +0900

    2935 문제의 개선된 풀이 완료

--- baekjoon_2935/main.cpp
+++ baekjoon_2935/main.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
-#include <string>
+#include <string.h>
+// strlen 함수는 string 헤더엔 없지만 string.h 헤더엔 존재함
 using namespace std;
-
-string a, b;
-char x;
-
-void func(string a, string b) {
-	for (int i = 0; i < a.size() - b.size(); i++) cout << a.at(i);
-	for (int i = 0; i < b.size(); i++) cout << b.at(i);
-}
+char x, a[101], b[101];
 
 int main() {
 	cin >> a >> x >> b;
 	if (x == '+') {
-		if (a.size() == b.size()) for (int i = 0; i < a.size(); i++) cout << (a.at(i) + b.at(i) - 2 * '0');
-		else if (a.size() > b.size()) func(a, b);
-		else func(b, a);
-	}
-	else {
-		int s = (a.at(0) - '0')*(b.at(0) - '0');
-		cout << s;
-		for (int i = 0; i < a.size() + b.size() - 2; i++) cout << 0;
+		int r = strlen(a) > strlen(b) ? strlen(a) : strlen(b);
+		for (int i = 0; i < r; i++) cout << (r - strlen(a) == i) + (r - strlen(b) == i);
+	} else {
+		cout << 1;
+		for (int i = 0; i < strlen(a) + strlen(b) - 2; i++) cout << 0;
 	}
 	return 0;
 }
